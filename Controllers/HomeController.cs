4c4504f17d44d5e3225b0c3ad199394ae32e8138
LearnerDotnet <41544507+LearnerDotnet@users.noreply.github.com>
2018-07-25 08:14:42 -0500

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(int? page)
+        {
+            var records = Enumerable.Range(1, 150).Select(x => "Record " + x);
+            var pagination = new Pagination(records.Count(), page);
+            var viewModel = new IndexViewModel { Items = records.Skip((pagination.CurrentPage - 1) * pagination.PageSize).Take(pagination.PageSize), Pagination = pagination };
+            return View(viewModel);
+        }
+    }
+}
